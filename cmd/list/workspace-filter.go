0d18a6576c15d8e23b8b76368f0ce415e46ff470
Jeff McCollum <jmccollum@hashicorp.com>
2022-12-09 13:44:21 -0600

    add workspace includes as well

--- cmd/list/workspace-filter.go
+++ cmd/list/workspace-filter.go
@@ -12,7 +12,7 @@ var (
 	tagsString        string
 	excludedTags      string
 	wildcardName      string
-	//workspaceIncludes tfe.WSIncludeOpt
+	includes []string
 
 	workspaceFilterCmd = &cobra.Command{
 		Use:     "workspace-filter",
@@ -34,12 +34,18 @@ func init() {
 	workspaceFilterCmd.PersistentFlags().StringVar(&tagsString, "tags", "", "comma-separated tag names used to filter the results")
 	workspaceFilterCmd.PersistentFlags().StringVar(&excludedTags, "excluded-tags", "", "comma-separated tag names to exclude")
 	workspaceFilterCmd.PersistentFlags().StringVar(&wildcardName, "wildcard-name", "", "workspace name to match with a wildcard")
-	//workspaceFilterCmd.PersistentFlags().StringSlice("workspace-includes", []string{}, "Additional relations to include")
+	workspaceFilterCmd.PersistentFlags().StringSliceVar(&includes, "includes", nil, "Additional relations to include, comma separated, no space")
 }
 
 func workspaceFilter(c tfclient.ClientContexts) error {
 
 	allItems := []*tfe.Workspace{}
+
+	// converts the type of slice from []string to []tfe.WSIncludeOpt Not sure if there is a way to not need this?
+	workspaceIncludes := make([]tfe.WSIncludeOpt, len(includes))
+	for i, v := range includes {
+		workspaceIncludes[i] = tfe.WSIncludeOpt(v)
+	}
 
 	workspaceFilterOpts := tfe.WorkspaceListOptions{
 		ListOptions:  tfe.ListOptions{PageNumber: 1, PageSize: 100},
@@ -47,8 +53,7 @@ func workspaceFilter(c tfclient.ClientContexts) error {
 		Tags:         tagsString,
 		ExcludeTags:  excludedTags,
 		WildcardName: wildcardName,
-		//Include:      workspaceIncludes,
-		//Include:      workspaceIncludes,
+		Include:      workspaceIncludes,
 	}
 
 	for {
@@ -69,5 +74,4 @@ func workspaceFilter(c tfclient.ClientContexts) error {
 	fmt.Print(len(allItems))
 
 	return nil
-
 }
