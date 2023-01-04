021d533f121197397d04a47930198ad464a2efbe
Joshua Tracy <joshua.tracy@hashicorp.com>
2023-01-04 15:20:34 -0500

    working

--- cmd/copy/workspaces-teamaccess.go
+++ cmd/copy/workspaces-teamaccess.go
@@ -8,8 +8,8 @@ import (
 	"github.com/pkg/errors"
 )
 
-func discoverSrcWsTeamAccess(c tfclient.ClientContexts, wsId string) ([]*tfe.TeamAccess, error) {
-	o.AddMessageUserProvided("Getting workspace team access permissions from source workspace ", wsId)
+func discoverSrcWsTeamAccess(c tfclient.ClientContexts, wsId string, wsName string) ([]*tfe.TeamAccess, error) {
+	o.AddMessageUserProvided("Getting workspace team access permissions from source workspace ", wsName)
 	srcTeamAccess := []*tfe.TeamAccess{}
 
 	opts := tfe.TeamAccessListOptions{
@@ -36,8 +36,8 @@ func discoverSrcWsTeamAccess(c tfclient.ClientContexts, wsId string) ([]*tfe.Tea
 	return srcTeamAccess, nil
 }
 
-func discoverDestWsTeamAccess(c tfclient.ClientContexts, wsId string) ([]*tfe.TeamAccess, error) {
-	o.AddMessageUserProvided("Getting workspace team access permissions from source workspace ", wsId)
+func discoverDestWsTeamAccess(c tfclient.ClientContexts, wsId string, wsName string) ([]*tfe.TeamAccess, error) {
+	o.AddMessageUserProvided("Getting workspace team access permissions from destination workspace ", wsName)
 	destTeamAccess := []*tfe.TeamAccess{}
 
 	opts := tfe.TeamAccessListOptions{
@@ -67,59 +67,24 @@ func discoverDestWsTeamAccess(c tfclient.ClientContexts, wsId string) ([]*tfe.Te
 func getSrcTeamAccessName(c tfclient.ClientContexts, srcteamId string) (string, error) {
 	var srcTeamName string
 
-	//srcTeams, err := discoverSrcTeamsNameFilter(tfclient.GetClientContexts())
-	//if err != nil {
-	//	fmt.Println("failed to list teams from source")
-	//}
-
-	//for _, srcteams := range srcTeams {
 	t, err := c.SourceClient.Teams.Read(c.SourceContext, srcteamId)
 	if err != nil {
 		return "", err
 	}
 
 	srcTeamName = t.Name
-	//}
 
 	return srcTeamName, nil
 }
 
-func discoverSrcTeamsNameFilter(c tfclient.ClientContexts) ([]*tfe.Team, error) {
-	o.AddMessageUserProvided("Getting list of teams from: ", c.SourceHostname)
-	srcTeams := []*tfe.Team{}
-
-	opts := tfe.TeamListOptions{
-		ListOptions: tfe.ListOptions{PageNumber: 1, PageSize: 100},
-		Include:     []tfe.TeamIncludeOpt{},
-	}
-	for {
-		items, err := c.SourceClient.Teams.List(c.SourceContext, c.SourceOrganizationName, &opts)
-		if err != nil {
-			return nil, err
-		}
-
-		srcTeams = append(srcTeams, items.Items...)
-
-		o.AddFormattedMessageCalculated("Found %d Teams", len(srcTeams))
-
-		if items.CurrentPage >= items.TotalPages {
-			break
-		}
-		opts.PageNumber = items.NextPage
-
-	}
-
-	return srcTeams, nil
-}
-
-func discoverDestTeamsNameFilter(c tfclient.ClientContexts, wsname string) ([]*tfe.Team, error) {
-	o.AddMessageUserProvided("Getting list of teams from: ", c.DestinationHostname)
+func discoverDestTeamsNameFilter(c tfclient.ClientContexts, teamName string) ([]*tfe.Team, error) {
+	//o.AddMessageUserProvided("Getting list of teams from: ", c.DestinationHostname)
 	destTeams := []*tfe.Team{}
 
 	opts := tfe.TeamListOptions{
 		ListOptions: tfe.ListOptions{PageNumber: 1, PageSize: 100},
 		Include:     []tfe.TeamIncludeOpt{},
-		Names:       []string{wsname},
+		Names:       []string{teamName},
 	}
 	for {
 		items, err := c.DestinationClient.Teams.List(c.DestinationContext, c.DestinationOrganizationName, &opts)
@@ -129,7 +94,7 @@ func discoverDestTeamsNameFilter(c tfclient.ClientContexts, wsname string) ([]*t
 
 		destTeams = append(destTeams, items.Items...)
 
-		o.AddFormattedMessageCalculated("Found %d Teams", len(destTeams))
+		//o.AddFormattedMessageCalculated("Found %d Teams", len(destTeams))
 
 		if items.CurrentPage >= items.TotalPages {
 			break
@@ -141,11 +106,11 @@ func discoverDestTeamsNameFilter(c tfclient.ClientContexts, wsname string) ([]*t
 	return destTeams, nil
 }
 
-func getDestTeamAccessNameAndID(c tfclient.ClientContexts, wsname string) (string, string, error) {
+func getDestTeamAccessNameAndID(c tfclient.ClientContexts, teamName string) (string, string, error) {
 	var destTeamName string
 	var destTeamId string
 
-	destTeams, err := discoverDestTeamsNameFilter(tfclient.GetClientContexts(), wsname)
+	destTeams, err := discoverDestTeamsNameFilter(tfclient.GetClientContexts(), teamName)
 	if err != nil {
 		fmt.Println("failed to list teams from destination")
 	}
@@ -167,13 +132,7 @@ func doesTeamNameMatch(srcTeamName string, destTeamName string) bool {
 	return srcTeamName == destTeamName
 }
 
-func doesTeamAccessPermissionsExist(c tfclient.ClientContexts, teamName string, destWorkspaceId string) (bool, error) {
-	// Get the source team access permissions for the destination workspace.
-	destTeamAccess, err := discoverDestWsTeamAccess(tfclient.GetClientContexts(), destWorkspaceId)
-	if err != nil {
-		return true, errors.Wrap(err, "failed to list Team Access for dest workspace")
-	}
-
+func doesTeamAccessPermissionsExist(c tfclient.ClientContexts, teamName string, destTeamAccess []*tfe.TeamAccess, destWorkspaceId string) (bool, error) {
 	var destTeamName string
 
 	for _, i := range destTeamAccess {
@@ -182,14 +141,13 @@ func doesTeamAccessPermissionsExist(c tfclient.ClientContexts, teamName string, 
 			return true, err
 		}
 		destTeamName = t.Name
-	}
 
-	fmt.Println("dest team access name from array is", destTeamName)
-
-	if teamName == string(destTeamName) {
-		return true, err
+		if teamName == string(destTeamName) {
+			return true, err
+		}
 	}
-	return false, err
+	return false, nil
+
 }
 
 func copyWsTeamAccess(c tfclient.ClientContexts) error {
@@ -212,80 +170,71 @@ func copyWsTeamAccess(c tfclient.ClientContexts) error {
 			return errors.New("Workspace not found")
 		}
 
-		// If Destination contians workspace with the same name as the source one, get the destination workspace ID
+		// Get the destination workspace ID
 		destWorkspaceId, err := getWorkspaceId(tfclient.GetClientContexts(), srcworkspace.Name)
 		if err != nil {
 			return errors.Wrap(err, "Failed to get the ID of the destination Workspace that matches the Name of the Source Workspace")
 		}
 
 		// Get the source team access permissions for the source workspace
-		srcTeamAccess, err := discoverSrcWsTeamAccess(tfclient.GetClientContexts(), srcworkspace.ID)
+		srcTeamAccess, err := discoverSrcWsTeamAccess(tfclient.GetClientContexts(), srcworkspace.ID, srcworkspace.Name)
 		if err != nil {
 			return errors.Wrap(err, "failed to list Team Access for source workspace")
 		}
 
-		// If The source team access permissions contians teams, get the source team names
-		for _, srcteamname := range srcTeamAccess {
+		destTeamAccess, err := discoverDestWsTeamAccess(tfclient.GetClientContexts(), destWorkspaceId, srcworkspace.Name)
+		if err != nil {
+			return errors.Wrap(err, "failed to list Team Access for dest workspace")
+		}
+
+		// If The source team access permissions contians teams, get the source team names filtering by Team ID
+		for _, srcteam := range srcTeamAccess {
 			if len(srcTeamAccess) > 0 {
-				srcTeamNames, err := getSrcTeamAccessName(tfclient.GetClientContexts(), srcteamname.Team.ID)
+				srcTeamNames, err := getSrcTeamAccessName(tfclient.GetClientContexts(), srcteam.Team.ID)
 				if err != nil {
 					return errors.Wrap(err, "failed to find source team name")
 				}
 
-				fmt.Println("srcTeamNames is ", srcTeamNames)
-
 				// Get the matching destination team names and their IDs
-				fmt.Println("team", srcTeamNames)
 				destTeamNames, destTeamId, err := getDestTeamAccessNameAndID(tfclient.GetClientContexts(), srcTeamNames)
 				if err != nil {
 					return errors.Wrap(err, "failed to find destination team name")
 				}
 
-				fmt.Println("DestTeamNames is ", destTeamNames, "for SrcTeamName", srcteamname)
-
 				// Ensure the team names match between the source and destination
 				match := doesTeamNameMatch(srcTeamNames, destTeamNames)
-				fmt.Println(match)
 				if match == true {
-					if err != nil {
-						return errors.Wrap(err, "failed to find destination team ID")
-					}
-					println("destTeamId is", destTeamId)
 					// Need to make a conditional for if "custom" permissions are set
 					// Loop through the team access for the source workspace
-					for _, teamaccess := range srcTeamAccess {
-						// For each team access setting, check for an existing access setting in the destination
-						exists, err := doesTeamAccessPermissionsExist(tfclient.GetClientContexts(), srcTeamNames, destWorkspaceId)
-						if err != nil {
-							return errors.Wrap(err, "failed to get destination permissions")
-						}
-						println("exists =", exists)
-						if exists {
-							fmt.Println("Exists in destination will not migrate", teamaccess.Team.Name)
-						} else {
-							teamaccess, err := c.DestinationClient.TeamAccess.Add(c.DestinationContext, tfe.TeamAccessAddOptions{
-								Type:   "",
-								Access: &teamaccess.Access,
-								//Runs:             &teamaccess.Runs,
-								//Variables:        &teamaccess.Variables,
-								//StateVersions:    &teamaccess.StateVersions,
-								//SentinelMocks:    &teamaccess.SentinelMocks,
-								//WorkspaceLocking: &teamaccess.WorkspaceLocking,
-								//RunTasks:         &teamaccess.RunTasks,
-								Team: &tfe.Team{
-									ID: destTeamId,
-								},
-								Workspace: &tfe.Workspace{
-									ID: destWorkspaceId,
-								},
-							})
+					// For each team access setting, check for an existing access setting in the destination
+					exists, err := doesTeamAccessPermissionsExist(tfclient.GetClientContexts(), srcTeamNames, destTeamAccess, destWorkspaceId)
+					if err != nil {
+						return errors.Wrap(err, "failed to get destination permissions")
+					}
+					if exists {
+						o.AddMessageUserProvided("Team exists in destination, skipping migration for: ", srcTeamNames)
+					} else {
+						teamaccess, err := c.DestinationClient.TeamAccess.Add(c.DestinationContext, tfe.TeamAccessAddOptions{
+							Type:   "",
+							Access: &srcteam.Access,
+							//Runs:             &teamaccess.Runs,
+							//Variables:        &teamaccess.Variables,
+							//StateVersions:    &teamaccess.StateVersions,
+							//SentinelMocks:    &teamaccess.SentinelMocks,
+							//WorkspaceLocking: &teamaccess.WorkspaceLocking,
+							//RunTasks:         &teamaccess.RunTasks,
+							Team: &tfe.Team{
+								ID: destTeamId,
+							},
+							Workspace: &tfe.Workspace{
+								ID: destWorkspaceId,
+							},
+						})
 
-							if err != nil {
-								return err
-							}
-							println("test2")
-							o.AddDeferredMessageRead("Migrated Team Access ", teamaccess.ID)
+						if err != nil {
+							return err
 						}
+						o.AddDeferredMessageRead("Migrated Team Access ", teamaccess.ID)
 					}
 				} else {
 					fmt.Println("Destination Team ID required to migrate Team Access, but none found")
