4ffe53be2cc74dec65b7baf7ec36233cd1456d02
Joshua Tracy <joshua.tracy@hashicorp.com>
2023-01-09 15:05:21 -0500

    stashing

--- cmd/copy/variablesets.go
+++ cmd/copy/variablesets.go
@@ -36,7 +36,11 @@ func init() {
 
 }
 
-func createVariableSets(c tfclient.ClientContexts) error {
+// Gets all variable sets in the source org and recreated them in the destination org.
+// Does not create variable set variables.
+// Also stores variable set ID for later use.
+func createVariableSets(c tfclient.ClientContexts) (string, error) {
+	var varSetID string
 
 	opts := tfe.VariableSetListOptions{
 		ListOptions: tfe.ListOptions{
@@ -50,7 +54,7 @@ func createVariableSets(c tfclient.ClientContexts) error {
 
 	if err != nil {
 		fmt.Println("Could not list variable sets.\n\n Error:", err.Error())
-		return err
+		return "", err
 	}
 
 	if set == nil {
@@ -73,6 +77,61 @@ func createVariableSets(c tfclient.ClientContexts) error {
 
 			if err != nil {
 				fmt.Println("Could not create variable set.\n\n Error:", err.Error())
+				return "", err
+			}
+
+			// Save the variable set ID for later.
+			varSetID := set.ID
+
+			return varSetID, err
+
+			o.AddDeferredMessageRead("Copied variable set: ", set.Name)
+		}
+	}
+	return varSetID, nil
+}
+
+// Gets all variables from the variable set ID provided in the source org and recreates them
+// in the destination org and variable set.
+func createVariableSetVars(c tfclient.ClientContexts, varSetID string) error {
+	opts := tfe.VariableSetVariableListOptions{
+		ListOptions: tfe.ListOptions{
+			PageNumber: 1,
+			PageSize:   100,
+		},
+	}
+
+	//Get all variables in the variable set
+	vars, err := c.SourceClient.VariableSetVariables.List(c.SourceContext, varSetID, &opts)
+
+	if err != nil {
+		fmt.Println("Could not list variables.\n\n Error:", err.Error())
+		return err
+	}
+
+	if vars == nil {
+		o.AddMessageUserProvided("No variables exist for variable set:", varSetName)
+
+	} else {
+		o.AddMessageUserProvided("Copying variable sets for org:", c.SourceOrganizationName)
+		for _, vars := range vars.Items {
+
+			//gather variable sets from source org.
+			varSet := tfe.VariableSetVariableCreateOptions{
+				Type:        "",
+				Key:         new(string),
+				Value:       new(string),
+				Description: new(string),
+				Category:    &"",
+				HCL:         new(bool),
+				Sensitive:   new(bool),
+			}
+
+			//Create the variable sets
+			_, err := c.DestinationClient.VariableSets.Create(c.DestinationContext, c.DestinationOrganizationName, &varSet)
+
+			if err != nil {
+				fmt.Println("Could not create variable set.\n\n Error:", err.Error())
 				return err
 			}
 
@@ -82,6 +141,9 @@ func createVariableSets(c tfclient.ClientContexts) error {
 	return nil
 }
 
+// Creates variable sets and then copies the variables from source set to destination set.
+
+// Copy variable sets - no variables
 func copyVariableSets(c tfclient.ClientContexts) error {
 	createVariableSets(c)
 	return nil
