b06c6fe4126d4d321fb56fd1cc3d294578b0d953
Jeff McCollum <jmccollum@hashicorp.com>
2023-01-18 10:25:33 -0600

    move side flag

--- cmd/list/list.go
+++ cmd/list/list.go
@@ -4,21 +4,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// `tfm list` commands
-var ListCmd = &cobra.Command{
-	Use:   "list",
-	Short: "List command",
-	Long:  "List objects in an org",
-}
+var (
+	side    string
+	
+	ListCmd = &cobra.Command{
+		Use:   "list",
+		Short: "List command",
+		Long:  "List objects in an org",
+	}
+)
 
 func init() {
-	// Here you will define your flags and configuration settings.
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// discoverCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// discoverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	ListCmd.PersistentFlags().StringVar(&side, "side", "", "Specify source or destination side to process")
 }
