1f451396dc69d3ba51d10640b27385b377f50a86
Jeff McCollum <jmccollum@hashicorp.com>
2023-05-08 14:09:30 -0500

    add delete command

--- cmd/root.go
+++ cmd/root.go
@@ -28,6 +28,7 @@ import (
 	"github.com/hashicorp-services/tfm/cmd/helper"
 	"github.com/hashicorp-services/tfm/cmd/list"
 	"github.com/hashicorp-services/tfm/cmd/nuke"
+	"github.com/hashicorp-services/tfm/cmd/delete"
 	"github.com/hashicorp-services/tfm/output"
 	"github.com/hashicorp-services/tfm/version"
 	"github.com/logrusorgru/aurora"
@@ -107,6 +108,7 @@ func init() {
 	RootCmd.AddCommand(copy.CopyCmd)
 	RootCmd.AddCommand(list.ListCmd)
 	RootCmd.AddCommand(nuke.NukeCmd)
+	RootCmd.AddCommand(delete.DeleteCmd)
 	// Turn off completion option
 	RootCmd.CompletionOptions.DisableDefaultCmd = true
 
