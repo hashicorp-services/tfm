42be3bc1652f8e699c2789264cb9d7ee246fcaf1
Joshua Tracy <joshua.tracy@hashicorp.com>
2023-08-30 15:22:58 -0400

    template additions

--- cmd/generate/config.go
+++ cmd/generate/config.go
@@ -46,25 +46,42 @@ src_tfe_token=""
 dst_tfc_hostname=""
 dst_tfc_org=""
 dst_tfc_token=""
-dst_tfc_project_id=""
+#dst_tfc_project_id=""
 
 # A list of source=destination VCS oauth IDs. TFM will look at each workspace in the source for the source VCS oauth ID and assign the matching workspace in the destination with the destination VCS oauth ID.
-vcs-map=[
-  "ot-wF6KZMna4desiPRc=ot-JSQTcnWxqVL5zQ1w",
-]
+#vcs-map=[
+#  "ot-wF6KZMna4desiPRc=ot-JSQTcnWxqVL5zQ1w",
+#]
 
 
 # A List of Workspaces to create/check are migrated across to new TFC
-"workspaces" = [
-  "example-ws-1",
-  "example-ws-2"
-]
+#"workspaces" = [
+#  "example-ws-1",
+#  "example-ws-2"
+#]
 
 # A list of source=destination workspace names. TFM will look at each source workspace and recreate the workspace with the specified destination name.
-"workspaces-map" = [
-  "example-ws-1=new-ws-1",
-  "example-ws-2=new-ws-2"
-    ]
+#"workspaces-map" = [
+#  "example-ws-1=new-ws-1",
+#  "example-ws-2=new-ws-2"
+#    ]
+
+# A list of source=destination agent pool IDs TFM will look at each workspace in the source for the source agent pool ID and assign the matching workspace in the destination the destination agent pool ID.
+#agents-map = [
+#  "apool-DgzkahoomwHsBHcJ=apool-vbrJZKLnPy6aLVxE",
+#  "apool-DgzkahoomwHsBHc3=apool-vbrJZKLnPy6aLVx4",
+#]
+
+# A list of source=destination variable set names. TFM will look at each source variable set and recreate the variable set with the specified destination name.
+#varsets-map = [
+#  "Azure-creds=New-Azure-Creds",
+#  "aws-creds2=New-AWS-Creds"
+# ]
+
+# A list of source=destination SSH IDs. TFM will look at each workspace in the source for the source SSH  ID and assign the matching workspace in the destination with the destination SSH ID.
+#ssh-map=[
+#  "sshkey-sPLAKMcqnWtHPSgx=sshkey-CRLmPJpoHwsNFAoN"
+#]
 `
 
 type TemplateData struct {
@@ -77,7 +94,7 @@ func generateConfigTemplate() {
 		panic(err)
 	}
 
-	outputFile, err := os.Create(".tfm.hcl2")
+	outputFile, err := os.Create(".tfm.hcl")
 	if err != nil {
 		panic(err)
 	}
