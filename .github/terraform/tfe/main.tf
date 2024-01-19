6a09376b0c8361ce0ee31c3c6f97f599b8eb6fe5
Jeff McCollum <jmccollum@hashicorp.com>
2024-01-19 16:06:13 -0600

    undo

--- .github/terraform/tfe/main.tf
+++ .github/terraform/tfe/main.tf
@@ -1,13 +1,13 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-# data "tfe_organization" "org" {
-#   name = var.organization
-# }
+data "tfe_organization" "org" {
+  name = var.organization
+}
 
 resource "tfe_workspace" "ci-workspace-test" {
   name         = "ci-workspace-test"
-  organization = var.source_tfe_organization
+  organization = data.tfe_organization.org.name
   tag_names    = ["test", "ci", "a"]
 }
 
