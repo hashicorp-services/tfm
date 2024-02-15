f68dd2d090afa9060d102d1c597d3bb91fc0bd0e
Joshua Tracy <joshua.tracy@hashicorp.com>
2024-02-15 08:38:47 -0500

    feat(oss-migration): Command added to cleanup cloned repos

--- cmd/oss/oss.go
+++ cmd/oss/oss.go
@@ -10,24 +10,10 @@ import (
 // `tfm oss` commands
 var OssCmd = &cobra.Command{
 	Use:   "oss",
-	Short: "oss command",
+	Short: "Command used to perform terraform open source (OSS) to TFE/TFC migration commands",
 	Long:  "Command used to perform terraform open source (OSS) to TFE/TFC migration commands",
 }
 
 func init() {
 
 }
-
-// Tfm oss clone-repos
-// Tfm oss init-repos
-// Tfm oss create-workspaces
-// Tfm oss connect-vcs
-// Tfm lock workspaces
-// Tfm oss push-state
-// Tfm unlock workspaces
-// Tfm oss remove-backend
-
-// github {
-// 	github_token = "your_github_token_here"
-// 	github_organization = "optional_default_organization"
-//   }
