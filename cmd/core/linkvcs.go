703bdae41e9132fe8b5fb590952dd83c9c37ab2b
Joshua Tracy <joshua.tracy@hashicorp.com>
2024-02-19 20:26:55 -0500

    feat(linkvcs): Tfm core link-vcs support for monorepos and terraform ce workspaces

--- cmd/core/linkvcs.go
+++ cmd/core/linkvcs.go
@@ -4,9 +4,10 @@
 package core
 
 import (
+	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"
+	"io/ioutil"
+	"strings"
 
 	"github.com/hashicorp-services/tfm/tfclient"
 	"github.com/hashicorp/go-tfe"
@@ -31,61 +32,121 @@ func init() {
 	CoreCmd.AddCommand(LinkVCSCmd)
 }
 
-func LinkVCS(c tfclient.DestinationContexts, clonePath string) error {
-	vcsProviderID := viper.GetString("vcs_provider_id")
-	githubOrganization := viper.GetString("github_organization")
-
-	if c.DestinationOrganizationName == "" || c.DestinationHostname == "" || c.DestinationToken == "" || vcsProviderID == "" {
-		return fmt.Errorf("Destination TFC/TFE Organization, hostname, token, or vcs_provider_id not configued.")
-	}
-
-	o.AddFormattedMessageCalculated3("Using VCS Provider %s in org %s\n", vcsProviderID, c.DestinationOrganizationName)
-
-	err := filepath.Walk(clonePath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if info.IsDir() && path != clonePath {
-			tfstatePath := filepath.Join(path, ".terraform", "pulled_terraform.tfstate")
-			if _, err := os.Stat(tfstatePath); err != nil {
-				return nil
-			}
-
-			workspaceName := filepath.Base(path)
-			// Construct the GitHub repo URL; adjust as needed based on your repo naming conventions
-			repoIdentifier := fmt.Sprintf("%s/%s", githubOrganization, workspaceName)
-
-			o.AddFormattedMessageCalculated3("Linking repo %s to workspace %s.\n", repoIdentifier, workspaceName)
-
-			workspace, err := c.DestinationClient.Workspaces.Read(c.DestinationContext, c.DestinationOrganizationName, workspaceName)
-			if err != nil {
-				fmt.Printf("Workspace %s not found: %v\n", workspaceName, err)
-				return nil
-			}
-
-			// Attempt to create a VCS connection
-			_, err = c.DestinationClient.Workspaces.Update(c.DestinationContext, c.DestinationOrganizationName, workspace.Name, tfe.WorkspaceUpdateOptions{
-				Type: "",
-				VCSRepo: &tfe.VCSRepoOptions{
-					Branch:            new(string),
-					Identifier:        &repoIdentifier,
-					IngressSubmodules: new(bool),
-					OAuthTokenID:      &vcsProviderID,
-					TagsRegex:         new(string),
-					GHAInstallationID: new(string),
-				},
-			})
-			if err != nil {
-				fmt.Printf("Failed to link VCS for workspace %s: %v\n", workspaceName, err)
-			} else {
-				fmt.Printf("Successfully linked %s to workspace %s\n", repoIdentifier, workspaceName)
-			}
-		}
-		return nil
-	})
-
+// Loads the metadata file information for use
+func loadMetadataLinkVcs(metadataFile string) ([]RepoConfig, error) {
+	var metadata []RepoConfig
+	file, err := ioutil.ReadFile(metadataFile)
 	if err != nil {
-		return fmt.Errorf("error processing directories: %v", err)
+		return nil, fmt.Errorf("error reading metadata file: %v", err)
 	}
+	err = json.Unmarshal(file, &metadata)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling metadata: %v", err)
+	}
+	return metadata, nil
+}
+
+// Constructs the workspace names with the same function used in tfm core create-workspaces for mapping of workspaces to VCS repos
+func constructWorkspaceNames(repoConfig RepoConfig, configPath ConfigPathInfo) []string {
+	var workspaceNames []string
+	basePath := strings.TrimPrefix(configPath.Path, repoConfig.RepoName+"/")
+
+	// TFC/TFE workspace names cannot contain '/' so we remove it
+	basePath = strings.ReplaceAll(basePath, "/", "-")
+
+	isRootConfigPath := basePath == "" || basePath == repoConfig.RepoName
+	basePathForWorkspaceNaming := ""
+	if !isRootConfigPath {
+		basePathForWorkspaceNaming = "-" + basePath
+	}
+
+	if configPath.WorkspaceInfo.UsesWorkspaces {
+		for _, workspaceName := range configPath.WorkspaceInfo.WorkspaceNames {
+
+			// Skip the default workspace if it's not the only one
+			if workspaceName == "default" && len(configPath.WorkspaceInfo.WorkspaceNames) > 1 {
+				continue
+			}
+
+			var fullWorkspaceName string
+
+			// Use repo name directly for default workspace at root
+			if isRootConfigPath && workspaceName == "default" {
+				fullWorkspaceName = repoConfig.RepoName
+			} else {
+				fullWorkspaceName = fmt.Sprintf("%s%s-%s", repoConfig.RepoName, basePathForWorkspaceNaming, workspaceName)
+			}
+			workspaceNames = append(workspaceNames, strings.Trim(fullWorkspaceName, "-"))
+		}
+	} else {
+
+		// For configurations that do not use multiple workspaces
+		fullWorkspaceName := repoConfig.RepoName + basePathForWorkspaceNaming
+		workspaceNames = append(workspaceNames, strings.Trim(fullWorkspaceName, "-"))
+	}
+
+	return workspaceNames
+}
+
+func LinkVCS(c tfclient.DestinationContexts, clonePath string) error {
+
+	// Check for necessary configurations
+	metadata, err := loadMetadataLinkVcs("terraform_config_metadata.json")
+	if err != nil {
+		return fmt.Errorf("error loading metadata: %v", err)
+	}
+
+	vcsProviderID := viper.GetString("vcs_provider_id")
+	if vcsProviderID == "" {
+		return fmt.Errorf("vcs_provider_id is not configured")
+	}
+
+	githubOrganization := viper.GetString("github_organization")
+	if githubOrganization == "" {
+		return fmt.Errorf("github_organization is not configured")
+	}
+
+	for _, repoConfig := range metadata {
+		for _, configPath := range repoConfig.ConfigPaths {
+			workspaceNames := constructWorkspaceNames(repoConfig, configPath)
+			for _, workspaceName := range workspaceNames {
+				repoIdentifier := fmt.Sprintf("%s/%s", githubOrganization, repoConfig.RepoName)
+				workingDirectory := ""
+
+				// Only set the working directory if config_path is not the root of the repo
+				if !(configPath.Path == repoConfig.RepoName || strings.Trim(configPath.Path, "/") == "") {
+					workingDirectory = strings.TrimPrefix(configPath.Path, repoConfig.RepoName+"/")
+				}
+
+				fmt.Printf("Linking repo %s to workspace %s with working directory %s.\n", repoIdentifier, workspaceName, workingDirectory)
+
+				workspace, err := c.DestinationClient.Workspaces.Read(c.DestinationContext, c.DestinationOrganizationName, workspaceName)
+				if err != nil {
+					fmt.Printf("Workspace %s not found: %v\n", workspaceName, err)
+					continue
+				}
+
+				updateOptions := tfe.WorkspaceUpdateOptions{
+					VCSRepo: &tfe.VCSRepoOptions{
+						Identifier:   &repoIdentifier,
+						OAuthTokenID: &vcsProviderID,
+					},
+				}
+
+				// Conditionally add the WorkingDirectory if it's not empty
+				if workingDirectory != "" {
+					updateOptions.WorkingDirectory = &workingDirectory
+				}
+
+				_, err = c.DestinationClient.Workspaces.Update(c.DestinationContext, c.DestinationOrganizationName, workspace.Name, updateOptions)
+				if err != nil {
+					fmt.Printf("[ERROR] Failed to link VCS for workspace %s: %v\n", workspaceName, err)
+				} else {
+					fmt.Printf("Successfully linked %s to workspace %s with working directory %s\n", repoIdentifier, workspaceName, workingDirectory)
+				}
+			}
+		}
+	}
+
 	return nil
 }
