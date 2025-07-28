1e967158a69e55de43bebc0468d89c0d2702fdff
Benjamin lykins <91494226+benjamin-lykins@users.noreply.github.com>
2025-07-28 16:47:13 -0400

    Set User-Agent header for source and destination TFE clients (#321)
    * Set User-Agent header for source and destination TFE clients
    
    * Refactor User-Agent header assignment to use constant

--- tfclient/tfclient.go
+++ tfclient/tfclient.go
@@ -15,6 +15,10 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	userAgent = "tfm"
+)
+
 type ClientContexts struct {
 	SourceClient                *tfe.Client
 	SourceContext               context.Context
@@ -93,9 +97,11 @@ func GetClientContexts() ClientContexts {
 		Address:           "https://" + viper.GetString("src_tfe_hostname"),
 		Token:             viper.GetString("src_tfe_token"),
 		RetryServerErrors: true,
+		Headers:           make(http.Header),
 		RetryLogHook: func(attemptNum int, resp *http.Response) {
 		},
 	}
+	sourceConfig.Headers.Set("User-Agent", userAgent)
 
 	sourceClient, err := createSrcClientWithRetry(sourceConfig, maxRetries, initialBackoff)
 	if err != nil {
@@ -107,9 +113,11 @@ func GetClientContexts() ClientContexts {
 		Address:           "https://" + viper.GetString("dst_tfc_hostname"),
 		Token:             viper.GetString("dst_tfc_token"),
 		RetryServerErrors: true,
+		Headers:           make(http.Header),
 		RetryLogHook: func(attemptNum int, resp *http.Response) {
 		},
 	}
+	destinationConfig.Headers.Set("User-Agent", userAgent)
 
 	destinationClient, err := createDestClientWithRetry(destinationConfig, maxRetries, initialBackoff)
 	if err != nil {
